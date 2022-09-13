2ccb1f36d2f7f136e54948a82ac25d7263565067
IYuliia <catnew2012@gmail.com>
2022-09-14 00:07:56 +0200

    added Weather component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      
-        <h1>Weather App</h1>
+       
+         <Weather />
         <footer>
           This project was coded by Yuliia Ihosheva and is 
        <a href="https://github.com/IYuliia/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> 
