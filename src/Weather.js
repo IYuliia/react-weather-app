390941991e932a33a879d899d447e6b6663910e3
IYuliia <catnew2012@gmail.com>
2022-09-14 00:23:12 +0200

    added the structure to the weather.js and css

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,88 @@ import React from 'react';
 import './Weather.css';
 
 export default function Weather() {
-    return <div className="Weather"> Hello </div>
+   let weatherData = {
+    city: "New York",
+    date: "Tuesday 10:08",
+    description: "Clear Sky",
+    temperature: "26",
+    imgUrl: "https://www.freeiconspng.com/images/sun",
+    pressure: "1017",
+    humidity: "17",
+    wind: "7"
+  };
+
+  return (
+    <div>
+      <div className="Weather">
+        <form className="search-form" id="search-form">
+          <div className="row">
+            <div className="col-6">
+              <input
+                type="search"
+                placeholder="Type a city..."
+                autofocus="on"
+                autoComplete="off"
+                className="form-control shadow-sm"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="form-control btn btn-outline-primary shadow-sm"
+              />
+            </div>
+            <div className="col-3">
+              <button className="btn btn-outline-secondary w-100">
+                Current Location
+              </button>
+            </div>
+          </div>
+        </form>
+
+        <h1> {weatherData.city}</h1>
+
+        <p>
+          {weatherData.date}
+
+          <div id="description"> </div>
+          <img src={weatherData.imgUrl} alt={weatherData.description} />
+        </p>
+
+        <h2>
+          <div className="weather-temperature">
+            {weatherData.temperature}
+            <span className="units">˚C</span>
+          </div>
+        </h2>
+        <hr />
+        <div className="buttons">
+          <div className="row">
+            <div className="col-4">
+              <span className="badge rounded-pill bg-primary">
+                PRESSURE:
+                <br />
+                {weatherData.pressure}
+              </span>
+            </div>
+
+            <div className="col-4">
+              <span className="badge rounded-pill bg-primary">
+                HUMIDITY: <br />
+                {weatherData.humidity} %
+              </span>
+            </div>
+            <div className="col-4">
+              <span className="badge rounded-pill bg-primary">
+                WIND SPEED: <br />
+                {weatherData.wind} km/h
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr />
+    </div>
+  );
 }
