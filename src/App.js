f2d963717a0515e00c37064b660cd93c2d16d14a
IYuliia <catnew2012@gmail.com>
2022-09-13 23:45:34 +0200

    added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React from 'react';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       
-        Weather App
-      
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Yuliia Ihosheva and is 
+       <a href="https://github.com/IYuliia/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub </a> 
+    </footer>
     </div>
   );
 }
